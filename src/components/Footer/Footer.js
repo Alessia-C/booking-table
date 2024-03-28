e2b2ad44513656c4bc32befe51c092b4f6d44323
Alessia-C <caporasoalessia1611@gmail.com>
2024-03-28 11:54:28 +0100

    fix footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,17 @@
 import React from "react";
 import logo from "../../assets/img/Logo.svg";
 import "./footer.css";
+import Nav from "../Nav";
 
 const Footer = () => {
   return (
     <footer>
-      <img src={logo} alt="logo little lemon restaurant" />
+      <img src={logo} alt="logo little lemon restaurant" aria-label="Little Lemon Restaurant Logo" />
       <div className="wrap-col">
+        <div className="col">
+          <h4>Navigation</h4>
+          <Nav />
+        </div>
         <div className="col">
           <h4>Contact</h4>
           <p>333 333 3333</p>
