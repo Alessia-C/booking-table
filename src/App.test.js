e2f431ad3ed579d44539b1d4b69596ec0aec655f
Alessia-C <caporasoalessia1611@gmail.com>
2025-04-27 20:08:21 +0200

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import Header from "./components/Header/Header";
-
-test("Renders the Form heading", () => {
-  render(<Header />);
-  const headingElement = screen.getByText("Book Your Table");
-  expect(headingElement).toBeInTheDocument();
-});
